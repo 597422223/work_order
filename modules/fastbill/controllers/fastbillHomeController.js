0d38852bf6b42fe0efb4d60456d1587cdd85ca57
597422223 <597422223@qq.com>
2016-10-12 13:42:54 +0800

    赵飞翔

--- modules/fastbill/controllers/fastbillHomeController.js
+++ modules/fastbill/controllers/fastbillHomeController.js
@@ -1,7 +1,7 @@
 var app=angular.module("myApp",[]);
 app.controller("FastCtrl",['$scope','$rootScope','$http','RequestService',"myService",function($scope,$rootScope,$http,RequestService,myService){
 	var fastbill=function(){
-		var fastAry=[];//项目
+		$rootScope.fastAry=[];//项目
 		var fastNameary=[];//项目名称
 		var jsondata={};
 		var unmInedx;
@@ -12,35 +12,44 @@ app.controller("FastCtrl",['$scope','$rootScope','$http','RequestService',"mySer
 			'carDis':2.4,
 		});
 		$scope.data=RequestService.ReturnData($scope.RequestUrl,$scope.Parameter);
+
 		$scope.data.success(function(data){
-				$scope.fastName=data.data.list;						
+				$scope.fastName=data.data.list;
+
 				$scope.fast_project=function(index){
-					fastNameary.push(data.data.list[index].projectName);				
+					fastNameary.push(data.data.list[index].projectName);
 					$.each(fastNameary,function(key,value){
 						if(!jsondata[fastNameary[key]]){
-		                   	fastAry.push(data.data.list[index]);
-		                   jsondata[fastNameary[key]]=1;
-		                   unmInedx=1;
-	               		}
+							$rootScope.fastAry.push(data.data.list[index]);
+							jsondata[fastNameary[key]]=1;
+							unmInedx=1;
+						}
 					});
 					var isActive = 'isActive'+index;
 					if($rootScope[isActive] )
 					{
 						$rootScope[isActive] = '';
+
+						fastAry.splice(index,1);
+						console.log(index);
+						console.log($rootScope.fastAry);
 					}
 					else
 					{
 						$rootScope[isActive] = 'list-border';
+						// $.each(fastNameary,function(key,value){
+						// 	if(!jsondata[fastNameary[key]]){
+						// 		fastAry.push(data.data.list[index]);
+						// 		jsondata[fastNameary[key]]=1;
+						// 	}
+						// 	fastAry.splice(index,1);
+						// })
 					}
 
-					myService.set(fastAry);	
+					myService.set($rootScope.fastAry);
 				}
 		});
 		
 	};
 	fastbill();
-
-
-
-	
 }]);
