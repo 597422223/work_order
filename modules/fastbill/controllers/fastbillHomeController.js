00722b211357d2ec8f5cd12ed05eb02facce52cd
597422223 <597422223@qq.com>
2016-10-13 11:49:43 +0800

    赵飞翔

--- modules/fastbill/controllers/fastbillHomeController.js
+++ modules/fastbill/controllers/fastbillHomeController.js
@@ -1,54 +1,51 @@
 var app=angular.module("myApp",[]);
 app.controller("FastCtrl",['$scope','$rootScope','$http','RequestService',"myService",function($scope,$rootScope,$http,RequestService,myService){
-	var fastbill=function(){
-		$rootScope.fastAry=[];//项目
-		var fastNameary=[];//项目名称
-		var jsondata={};
+	var fastbill=function() {
+		var fastAry = [];//项目
+		var fastNameary = [];//项目名称
+		var jsondata = {};
 		var unmInedx;
-		$scope.RequestUrl ='/customer/project/quick';
+		$scope.RequestUrl = '/customer/project/quick';
 		$scope.Parameter = $.param({
-			"token":"3c25a936a9e06c16f62cecb0d13c6282",
-			'localCarId':18,
-			'carDis':2.4,
+			"token": "3c25a936a9e06c16f62cecb0d13c6282",
+			'localCarId': 18,
+			'carDis': 2.4,
 		});
-		$scope.data=RequestService.ReturnData($scope.RequestUrl,$scope.Parameter);
+		$scope.data = RequestService.ReturnData($scope.RequestUrl, $scope.Parameter);
 
-		$scope.data.success(function(data){
-				$scope.fastName=data.data.list;
+		$scope.data.success(function (data) {
+			$scope.fastName = data.data.list;
 
-				$scope.fast_project=function(index){
-					fastNameary.push(data.data.list[index].projectName);
-					$.each(fastNameary,function(key,value){
-						if(!jsondata[fastNameary[key]]){
-							$rootScope.newdata.data.push(data.data.list[index]);
-							jsondata[fastNameary[key]]=1;
+			$scope.fast_project = function (index) {
+				var unm=data.data.list[index];
+				fastNameary.push(data.data.list[index].projectName);
+					var isActive = 'isActive' + index;
+					if ($rootScope[isActive]) {
+						$rootScope[isActive] = '';
+						$rootScope.haveClass = 0;
+					}
+					else {
+						$rootScope[isActive] = 'list-border';
+						$rootScope.haveClass = 1;
+					}
+					if ($rootScope.haveClass = 1) {
+						fastAry.splice(index, 1);
+					}
+					$(document).on("click","#FastBillin .list-FastBilling",function (index) {
+						// console.log($(this).hasClass("list-border"));
+						if ($(this).hasClass("list-border")){
+							$.each(fastNameary, function (key, value) {
+								if (!jsondata[fastNameary[key]]) {
+									fastAry.push(unm);
+									jsondata[fastNameary[key]] = 1;
+								}});
+							console.log(fastAry);
 						}
 					});
-					var isActive = 'isActive'+index;
-					if($rootScope[isActive] )
-					{
-						$rootScope[isActive] = '';
 
-						$rootScope.newdata.data.splice(index,1);
-						console.log(index);
-						console.log($rootScope.fastAry);
-					}
-					else
-					{
-						$rootScope[isActive] = 'list-border';
-						// $.each(fastNameary,function(key,value){
-						// 	if(!jsondata[fastNameary[key]]){
-						// 		fastAry.push(data.data.list[index]);
-						// 		jsondata[fastNameary[key]]=1;
-						// 	}
-						// 	fastAry.splice(index,1);
-						// })
-					}
-
-					myService.set($rootScope.fastAry);
-				}
+					// myService.set($rootScope.fastAry);
+			}
 		});
-		
-	};
+	}
 	fastbill();
 }]);
